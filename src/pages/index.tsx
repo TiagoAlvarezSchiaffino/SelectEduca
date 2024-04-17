f861a91be702f7726517e21aab4cbf2418e06945
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-04-17 12:42:33 -0300

    fix: Clarify and keep the button spinning while waiting for launchMeeting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,55 +1,52 @@
 import {
-    Avatar,
-    Box,
-    Button,
-    Card,
-    CardBody,
-    CardHeader,
-    Flex,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalBody,
-    Heading,
-    StackDivider,
-    Text,
-    VStack,
-    useColorModeValue,
-    FormLabel,
-    Input,
-    FormControl,
-  } from '@chakra-ui/react';
-  import React, { Fragment, useState } from 'react';
-  import { NextPageWithLayout } from "../NextPageWithLayout";
-  import AppLayout from "../layouts";
-  import useUserContext from "../useUserContext";
-  import tClientBrowser from "../tClientBrowser";
-  import tClientNext from "../tClientNext";
-  import { MdVideocam } from 'react-icons/md';
-  import { toast } from "react-toastify";
-  import pinyin from 'tiny-pinyin';
+  Avatar,
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  Heading,
+  StackDivider,
+  Text,
+  VStack,
+  useColorModeValue,
+  FormLabel,
+  Input,
+  FormControl,
+} from '@chakra-ui/react';
+import React, { Fragment, useState } from 'react';
+import { NextPageWithLayout } from "../NextPageWithLayout";
+import AppLayout from "../layouts";
+import useUserContext from "../useUserContext";
+import tClientBrowser from "../tClientBrowser";
+import tClientNext from "../tClientNext";
+import { MdVideocam } from 'react-icons/md';
+import { toast } from "react-toastify";
+import pinyin from 'tiny-pinyin';
+import { TRPCClientError } from '@trpc/client';
+import Link from 'next/link';
 
   
-  const Index: NextPageWithLayout = () => {
-    const [user] = useUserContext();
-    return <Box paddingTop={'80px'}> {user.name ? <></> : <SetNameModal />} <Meetings /></Box>
-  }
-  
-  Index.getLayout = (page) => <AppLayout>{page}</AppLayout>;
-  
-  export default Index;
-
-  function SetNameModal() {
-    const [user, setUser] = useUserContext();
-    const [isOpen, setOpen] = useState(true);
-    const [name, setName] = useState('');
-  
-    const handleSubmit = async () => {
-      if (name) {
-        const updatedUser = structuredClone(user);
-        updatedUser.name = name;
-  
+const Index: NextPageWithLayout = () => {
+  const [user] = useUserContext();
+  return <Box paddingTop={'80px'}> {user.name ? <></> : <SetNameModal />} <Meetings /></Box>
+}
+Index.getLayout = (page) => <AppLayout>{page}</AppLayout>;
+export default Index;
+function SetNameModal() {
+  const [user, setUser] = useUserContext();
+  const [isOpen, setOpen] = useState(true);
+  const [name, setName] = useState('');
+  const handleSubmit = async () => {
+    if (name) {
+      const updatedUser = structuredClone(user);
+      updatedUser.name = name;
       // TODO: Handle error display globally. Redact server-side errors.
       try {
         await tClientBrowser.me.updateProfile.mutate(updatedUser);
@@ -59,92 +56,105 @@ import {
       } catch (e) {
         toast.error((e as Error).message);
       }
-      };
     };
-  
-    return (
-    // onClose returns undefined to prevent user from closing the modal without entering name.
-    <Modal isOpen={isOpen} onClose={() => undefined}>
-        <ModalOverlay backdropFilter='blur(8px)' />
-        <ModalContent>
-          <ModalHeader> 👋</ModalHeader>
-          <ModalBody>
-            <Box mt={4}>
-              <FormControl>
+  };
+  return (
+  // onClose returns undefined to prevent user from closing the modal without entering name.
+  <Modal isOpen={isOpen} onClose={() => undefined}>
+      <ModalOverlay backdropFilter='blur(8px)' />
+      <ModalContent>
+        <ModalHeader> 👋</ModalHeader>
+        <ModalBody>
+          <Box mt={4}>
+            <FormControl>
               <FormLabel></FormLabel>
-                <Input
-                  isRequired={true}
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  placeholder=''
-                  mb='24px'
-                />
-                <Button
-                  onClick={handleSubmit}
-                  isDisabled={!isValidChineseName(name)}
-                  variant='brand' w='100%' mb='24px'>
-                  
-                </Button>
-              </FormControl>
-            </Box>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
-    );
-  }
+              <Input
+                isRequired={true}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder='请勿使用英文或其他符号'
+                mb='24px'
+              />
+              <Button
+                onClick={handleSubmit}
+                isDisabled={!isValidChineseName(name)}
+                variant='brand' w='100%' mb='24px'>
+                
+              </Button>
+            </FormControl>
+          </Box>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+}
+function isValidChineseName(s: string) : boolean {
+  return s.length >= 2 && pinyin.parse(s).every(token => token.type === 2);
+}
+function Meetings() {
+  const { data: groups, isLoading } = tClientNext.myGroups.list.useQuery();
+  const [user] = useUserContext();
+  return (
+    <Card>
+      <CardHeader>
+        <Heading size='md'></Heading>
+      </CardHeader>
+      <CardBody>
+        {!groups
+        && isLoading
+        && <Text align='center'>
+            ...
+        </Text>}
+        
+        {groups
+        && groups.length == 0
+        && !isLoading
+        && <Text align='center'>
 
-  function isValidChineseName(s: string) : boolean {
-    return s.length >= 2 && pinyin.parse(s).every(token => token.type === 2);
-  }
+           </Text>}
+        
+        <VStack divider={<StackDivider />} align='left' spacing='6'>
+          {groups &&
+            groups.map((group, idx) => 
+              <Meeting key={idx} userId={user.id} group={group} />)
+          }
+        </VStack>
+      </CardBody>
+    </Card>
+  );
+}
   
-  
-  function Meetings() {
-    const { data: groups, isLoading } = tClientNext.myGroups.list.useQuery();
-    const [user] = useUserContext();
-  
-    return (
-      <Card>
-        <CardHeader>
-          <Heading size='md'></Heading>
-        </CardHeader>
-        <CardBody>
-          {!groups
-          && isLoading
-          && <Text align='center'>
-              ...
-          </Text>}
-
-          {groups
-          && groups.length == 0
-          && !isLoading
-          && <Text align='center'>
-            
-            </Text>}
-
-          <VStack divider={<StackDivider />} align='left' spacing='6'>
-            {groups &&
-              groups.map((group, idx) => 
-                <Meeting key={idx} userId={user.id} group={group} />)
-            }
-          </VStack>
-        </CardBody>
-      </Card>
-    );
-  }
-  
-  // @ts-ignore type checking for props is anonying
-  function Meeting(props) {
+// @ts-ignore type checking for props is anonying
+function Meeting(props) {
     const transcriptCount = props.group.transcripts.length;
     const textColor = useColorModeValue('secondaryGray.700', 'white');
+    const [isJoiningMeeting, setJoining] = useState(false);
+    const launchMeeting = async (groupId: string) => {
+      setJoining(true);
+      try {
+        const link = await tClientBrowser.myGroups.generateMeetingLink.mutate({ groupId: groupId });
+        window.location.href = link;
+      } catch (e) {
+        toast.error((e as Error).message, { autoClose: false });
+      } finally {
+        // More time is needed to redirect to the meeting page. Keep it spinning.
+        // We should uncomment this line if we pop the page in a new window.
+        // setJoining(false);
+      }
+    }
+
     return (
       <Flex flexWrap='wrap' gap={4}>
         <VStack>
-          <Button variant='outline' leftIcon={<MdVideocam />} 
+          <Button variant='outline' leftIcon={<MdVideocam />}
+            isLoading={isJoiningMeeting} loadingText={'...'}
             onClick={async () => launchMeeting(props.group.id)}>
           </Button>
-          <Text color={textColor} fontSize='sm'>
-            {(transcriptCount ? `${transcriptCount} ` : '') + ''}
-          </Text>
+          <Link href={`/groups/${props.group.id}`}>
+            <Text color={textColor} fontSize='sm' >
+              {(transcriptCount ? `${transcriptCount} ` : '') + ''}
+            </Text>
+          </Link>
         </VStack>
         {
           props.group.users
@@ -158,9 +168,4 @@ import {
         }
       </Flex>
     );
-  }
-  
-  async function launchMeeting(groupId: string) {
-    const meetingLink = await tClientBrowser.myGroups.generateMeetingLink.mutate({ groupId: groupId });
-    window.location.href = meetingLink;
   }
