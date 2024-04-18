447ad74dc40846c86eb39692dec3c31f5d98bc2c
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-04-18 08:57:34 -0300

    chore: Disable Sequelize logging

--- src/api/database/sequelizeInstance.ts
+++ src/api/database/sequelizeInstance.ts
@@ -6,10 +6,11 @@ import Group from "./models/Group";
 import GroupUser from "./models/GroupUser";
 import Transcript from "./models/Transcript";
 import Summary from "./models/Summary";
+import OngoingMeetingCount from "./models/OngoingMeetingCount";
 
 const sequelizeInstance = new Sequelize(apiEnv.DATABASE_URI, {
-  models: [User, Group, GroupUser, Transcript, Summary],
-
+  models: [User, Group, GroupUser, Transcript, Summary, OngoingMeetingCount],
+  logging: false,
   dialectModule: require('pg'),
   retry: {
         // Error types: https://sequelize.org/api/v6/identifiers.html#errors
