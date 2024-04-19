6ae4a7642088b361cea1cf5287fbd2452e674404
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-04-19 08:20:40 -0300

    feat: Add ConsentModal and allow users to decline consent

---
+++ src/components/ConsentModal.tsx
@@ -0,0 +1,77 @@
+import {
+    Button,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalBody,
+    Text,
+    VStack,
+    ModalFooter,
+    Link,
+  } from '@chakra-ui/react';
+  import React, { useState } from 'react';
+  import useUserContext from "../useUserContext";
+  import tClientBrowser from "../tClientBrowser";
+  import { toast } from "react-toastify";
+  import moment from 'moment';
+  import UserProfile from '../shared/UserProfile';
+  
+  export function consentFormAccepted(user: UserProfile) {
+    return user.consentFormAcceptedAt && moment(user.consentFormAcceptedAt) >= moment("20111031", "YYYYMMDD");
+  }
+  
+  export default function ConsentModal() {
+    const [user, setUser] = useUserContext();
+    const [declined, setDeclined] = useState<boolean>(false);
+  
+    const handleSubmit = async () => {
+      const updatedUser = structuredClone(user);
+      updatedUser.consentFormAcceptedAt = new Date();
+  
+      try {
+        await tClientBrowser.me.updateProfile.mutate(updatedUser);
+        setUser(updatedUser);
+      } catch (e) {
+        toast.error((e as Error).message);
+      }
+    };
+  
+    const MyLink = (props: any) => <Link isExternal color='teal.500' {...props} />;
+  
+    return <>
+      {/* onClose returns undefined to prevent user from closing the modal without entering name. */}
+      <Modal isOpen={!declined} onClose={() => undefined}>
+        <ModalOverlay backdropFilter='blur(8px)' />
+        <ModalContent>
+          <ModalHeader></ModalHeader>
+          <ModalBody>
+            <VStack spacing={6} marginBottom={10} align='left'>
+              <Text><b></b></Text>
+  
+              <Text></Text>
+  
+              <Text></Text>
+            </VStack>
+          </ModalBody>
+          <ModalFooter>
+            <Button variant='ghost' onClick={() => setDeclined(true)}></Button>
+            <Button variant='brand' onClick={handleSubmit}></Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+  
+      <Modal isOpen={declined} onClose={() => undefined}>
+        <ModalOverlay backdropFilter='blur(8px)' />
+        <ModalContent>
+          <ModalHeader> </ModalHeader>
+          <ModalBody>
+            <Text></Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={() => setDeclined(false)}></Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>;
+  }
