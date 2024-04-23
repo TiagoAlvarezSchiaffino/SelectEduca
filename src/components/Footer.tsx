4e01b1c37b1a2abceaf0ca24c2debcf6788e4d3a
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-04-23 15:55:37 -0300

    refactor: Footer and replace tabs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,71 +7,71 @@ import Image from "next/image";
 export const bodyFooterSpacing = 80;
 
 export default function Footer() {
-	const textColor = useColorModeValue('gray.400', 'white');
-	return (
-		<Flex
-			zIndex='3'
-			flexDirection={{
-				base: 'column',
-				xl: 'row'
-			}}
-			alignItems={{
-				base: 'center',
-				xl: 'start'
-			}}
-			justifyContent='space-between'
-			paddingX={{ base: '30px', md: '50px' }}
-			paddingBottom='30px'
-            paddingTop={`${bodyFooterSpacing}px`}
+    const textColor = useColorModeValue('gray.400', 'white');
+    return (
+      <Flex
+        zIndex='3'
+        flexDirection={{
+          base: 'column',
+          xl: 'row'
+        }}
+        alignItems={{
+          base: 'center',
+          xl: 'start'
+        }}
+        justifyContent='space-between'
+        paddingX={{ base: '30px', md: '50px' }}
+        paddingBottom='30px'
+        paddingTop={`${bodyFooterSpacing}px`}
     >
-			<Text
-				color={textColor}
-				textAlign={{
-					base: 'center',
-					xl: 'start'
-				}}
-				paddingBottom={{ base: '20px', xl: '0px' }}>
-				{' '}
-				<Text as='span' fontWeight='500' marginStart='4px'>
-					&copy; {new Date().getFullYear()} 
-				</Text>
-			</Text>
-			<List display='flex'>
-				<ListItem
-					marginEnd={{
-						base: '20px',
-						md: '44px'
-					}}>
-					<Link fontWeight='500' color={textColor} target='_blank' href=''>
-						
-					</Link>
-				</ListItem>
-				<ListItem
-					marginEnd={{
-						base: '20px',
-						md: '44px'
-					}}>
-					<Link fontWeight='500' color={textColor} target='_blank' href=''>
-						
-					</Link>
-				</ListItem>
-				<ListItem
-					marginEnd={{
-						base: '20px',
-						md: '44px'
-					}}>
-					<Link fontWeight='500' color={textColor} target='_blank' href=''>
-						
-					</Link>
-				</ListItem>
+			      <Text
+        color={textColor}
+        textAlign={{
+          base: 'center',
+          xl: 'start'
+        }}
+        paddingBottom={{ base: '20px', xl: '0px' }}>
+        {' '}
+        <Text as='span' fontWeight='500' marginStart='4px'>
+          &copy; {new Date().getFullYear()} 
+        </Text>
+      </Text>
+      <List display='flex'>
+        <ListItem
+          marginEnd={{
+            base: '20px',
+            md: '44px'
+          }}>
+          <Link fontWeight='500' color={textColor} target='_blank' href=''>
+            
+          </Link>
+        </ListItem>
+        <ListItem
+          marginEnd={{
+            base: '20px',
+            md: '44px'
+          }}>
+          <Link fontWeight='500' color={textColor} target='_blank' href=''>
+            
+          </Link>
+        </ListItem>
+        <ListItem
+          marginEnd={{
+            base: '20px',
+            md: '44px'
+          }}>
+          <Link fontWeight='500' color={textColor} target='_blank' href=''>
+            
+          </Link>
+        </ListItem>
         <Center opacity='50%'>
           <ListItem>
               <Link target='_blank' href="">
-                <Image src={vercelBanner} alt="Banner" height="22" />
+                <Image src={} alt="Banner" height="22" />
               </Link>
           </ListItem>
         </Center>
-			</List>
-		</Flex>
-	);
+        </List>
+    </Flex>
+  );
 }
