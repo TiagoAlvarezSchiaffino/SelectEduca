20828494044f1c6da0d3dbedfbe2bb7f973fa6f8
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-04-24 12:06:59 -0300

    feat: Add NavBar component and remove unnecessary code

--- src/api/routes/groups.ts
+++ src/api/routes/groups.ts
@@ -12,7 +12,7 @@ import invariant from "tiny-invariant";
 import _ from "lodash";
 import { isPermitted } from "../../shared/Role";
 import sequelizeInstance from "../database/sequelizeInstance";
-import { formatGroupName, formatUserName } from "../../shared/formatNames";
+import { formatUserName, formatGroupName } from "../../shared/strings";
 import nzh from 'nzh';
 import { email } from "../sendgrid";
 
