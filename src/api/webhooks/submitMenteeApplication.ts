337fdea7446273875d25ecf7e0820e6ef07493fc
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-04-30 08:00:09 -0300

    fix: Support Mentee application form

--- src/api/webhooks/submitMenteeApplication.ts
+++ src/api/webhooks/submitMenteeApplication.ts
@@ -10,13 +10,13 @@ export default procedure
     .mutation(async ({ ctx, input }) => submit(input, ctx.baseUrl));
 
 export async function submit({ form, entry }: Record<string, any>, baseUrl: string) {
-  if (form !== "FBTWTe") {
-    throw generalBadRequestError();
+  if (form !== "FBTWTe" && form !== "S74k0V") {
+    throw generalBadRequestError(`form id ${form} is not suppoted.`);
   }
 
   const application: Record<string, any> = {};
   for (const field of menteeApplicationFields) {
-    const jn = field.jinshujuName;
+    const jn = form == "FBTWTe" ? field.jsjField : field.jsjProxiedField;
     if (jn && jn in entry) {
       application[field.name] = entry[jn];
     }
@@ -30,7 +30,7 @@ export async function submit({ form, entry }: Record<string, any>, baseUrl: stri
     wechat: entry.field_106,
     menteeApplication: application,
     roles: ["Mentee"]
-  });
+  }, "upsert");
 
   emailRoleIgnoreError("UserManager", "", `${name}`, baseUrl);
 }
