82aa038470df6848c0b1c16033b940d1145263a1
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-05-06 05:15:05 -0300

    feat: Add buttons theme

---
+++ src/theme/buttons.ts
@@ -0,0 +1,23 @@
+import { mode, StyleFunctionProps } from "@chakra-ui/theme-tools";
+import colors from "./colors";
+
+const buttons = {
+  components: {
+    Button: {
+      variants: {
+        brand: (props: StyleFunctionProps) => ({
+          bg: mode(colors.brand[500], colors.brand.c)(props),
+          color: "white",
+          _hover: {
+            bg: mode(colors.brand[600], colors.brand.c)(props),
+            _disabled: {
+              bg: mode(colors.brand[600], colors.brand.c)(props),
+            }
+          },
+        }),
+      },
+    },
+  },
+};
+
+export default buttons;
