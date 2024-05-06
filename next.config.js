2aff86577605b9ce4faabf9fe9be4fa9d51f0d2c
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-05-05 21:33:08 -0300

    feat: Add Sentry for monitoring

--- next.config.js
+++ next.config.js
@@ -6,3 +6,37 @@ const nextConfig = {
   };
   
   module.exports = nextConfig;
+
+
+// Injected content via Sentry wizard below
+
+const { withSentryConfig } = require("@sentry/nextjs");
+
+module.exports = withSentryConfig(
+  module.exports,
+  {
+
+    // Suppresses source map uploading logs during build
+    silent: true,
+
+    org: "visionary-education-foundation",
+    project: "javascript-nextjs",
+  },
+  {
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: true,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    tunnelRoute: "/monitoring",
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+  }
+);
