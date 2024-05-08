7846f8194531ca7b2c38bbe233244ecc6c1b251b
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-05-08 17:05:41 -0300

    refactor: Redo Summary Name Map model

--- src/api/database/db.ts
+++ src/api/database/db.ts
@@ -15,7 +15,7 @@ import Calibration from "./models/Calibration";
 import InterviewFeedbackUpdateAttempt from "./models/InterviewFeedbackUpdateAttempt";
 import ChatRoom from "./models/ChatRoom";
 import ChatMessage from "./models/ChatMessage";
-import TranscriptNameMapping from "./models/TranscriptNameMapping";
+import SummaryNameMap from "./models/SummaryNameMap";
 
 const db = {
   Partnership,
@@ -32,7 +32,7 @@ const db = {
   Calibration,
   ChatRoom,
   ChatMessage,
-  TranscriptNameMapping,
+  SummaryNameMap,
 };
 
 export default db;
--- src/api/routes/transcripts.ts
+++ src/api/routes/transcripts.ts
@@ -4,36 +4,24 @@ import { z } from "zod";
 import db from "../database/db";
 import { zTranscript } from "shared/Transcript";
 import { notFoundError } from "api/errors";
-import { groupAttributes, groupInclude, transcriptAttributes } from "api/database/models/attributesAndIncludes";
+import {
+  groupAttributes,
+  groupInclude,
+  transcriptAttributes,
+  summaryAttributes
+} from "api/database/models/attributesAndIncludes";
 import { checkPermissionForGroup } from "./groups";
-
-const get = procedure
-  .use(authUser())
-  .input(z.string())
-  .output(zTranscript)
-  .query(async ({ input: id, ctx }) =>
-{
-  const t = await db.Transcript.findByPk(id, {
-    attributes: transcriptAttributes,
-    include: [{
-      model: db.Group,
-      attributes: groupAttributes,
-      include: groupInclude,
-    }],
-  });
-
-  if (!t) throw notFoundError("", id);
-
-  checkPermissionForGroup(ctx.user, t.group);
-
-  return t;
-});
+import invariant from 'tiny-invariant';
+import { Op } from "sequelize";
+import Summary from "api/database/models/Summary";
 
 const list = procedure
   .use(authUser())
-  .input(z.string())
+  .input(z.object({
+    groupId: z.string(),
+  }))
   .output(z.array(zTranscript))
-  .query(async ({ input: groupId, ctx }) =>
+  .query(async ({ input: { groupId }, ctx }) =>
 {
   const g = await db.Group.findByPk(groupId, {
     attributes: groupAttributes,
@@ -43,15 +31,101 @@ const list = procedure
     }],
   });
 
-  if (!g) throw notFoundError("", groupId);
+  if (!g) throw notFoundError("Group", groupId);
 
   checkPermissionForGroup(ctx.user, g);
 
   return g.transcripts;
 });
 
-const transcripts = router({
-  get,
-  list,
+/**
+ * @return null if there is no transcript.
+ */
+const getMostRecentStartedAt = procedure
+  .use(authUser(["MentorCoach", "PartnershipManager"]))
+  .input(z.object({
+    groupId: z.string(),
+  }))
+  .output(z.date().nullable())
+  .query(async ({ input: { groupId } }) =>
+{
+  return await db.Transcript.max("startedAt", { where: { groupId } });
 });
-export default transcripts;
+
+const getNameMap = procedure
+  .use(authUser())
+  .input(z.object({ transcriptId: z.string() }))
+  .output(z.record(z.string()))
+  .query(async ({ input }) => 
+{
+  const { nameMap } = await getSummariesAndNameMap(input.transcriptId);
+  return nameMap;
+});
+
+/**
+ * @param { [handlebarNames]: userIds }
+ */
+const updateNameMap = procedure
+  .use(authUser())
+  .input(z.record(z.string()))
+  .mutation(async ({ input: nameMap }) => 
+{
+  // Construct an array of objects to upsert multiple rows the same time
+  const upsertArray = Object.entries(nameMap)
+    .map(([handlebarName, userId]) => ({
+      handlebarName,
+      userId,
+    }));
+
+  await db.SummaryNameMap.bulkCreate(upsertArray, { updateOnDuplicate: ['userId'] });
+});
+
+export default router({
+  list,
+  getMostRecentStartedAt,
+  getNameMap,
+  updateNameMap,
+});
+
+export async function getSummariesAndNameMap(transcriptId: string): Promise<{
+  summaries: Summary[],
+  nameMap: Record<string, string>,
+}> {
+  const summaries = await db.Summary.findAll({
+    where: { transcriptId },
+    attributes: summaryAttributes,
+  });
+
+  // find all handlebar from summaries under one transcript
+  let handlebarsSet = new Set<string>();
+
+  for (let s of summaries) {
+    const matches = s.summary.match(/{{(.*?)}}/g);
+    if (matches) {
+      const extracted = matches.map(match => match.slice(2, -2));
+      extracted.forEach(handlebar => handlebarsSet.add(handlebar));
+    }
+  }
+
+  const nameMap: Record<string, string> = {};
+  // create a list of namemap of itself, otherwise when Handlebars.js compile it will return empty
+  for (const handlebar of [...handlebarsSet]) {
+    nameMap[handlebar] = `**${handlebar}**`;
+  }
+
+  const snm = await db.SummaryNameMap.findAll({
+    where: { handlebarName: [...handlebarsSet] },
+    include: [{
+      model: db.User,
+      attributes: ['name'],
+      where: { name: { [Op.ne]: null } } // Ensure user's name is not null
+    }],
+  });
+
+  for (const nm of snm) {
+    invariant(nm.user.name);
+    nameMap[nm.handlebarName] = `**${nm.user.name}**`;
+  }
+
+  return { summaries, nameMap };
+}
