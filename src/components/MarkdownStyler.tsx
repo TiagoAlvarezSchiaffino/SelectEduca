de0bb922c74b99a024148657923481b6e3650f9a
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-06-19 23:40:24 -0300

    feat: Add MarkdownStyler to render using Chakra

---
+++ src/components/MarkdownStyler.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import styles from '../theme/MarkdownStyler.module.css';
+
+// This component is to correctly render Markdowns when using chakra UI
+// Chakra UI CSSReset component is turning down browser default style of elements
+const MarkdownStyler = ({ content }: { content: string }) => (
+  <div className={styles.markdownStyler}>
+    <ReactMarkdown>{content}</ReactMarkdown>
+  </div>
+);
+
+export default MarkdownStyler;
