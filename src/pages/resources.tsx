f8f9259f329351655b9b13fc447d2a317aab5baa
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-07-02 07:24:31 -0300

    fix: Make resource full height using variable

--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -1,14 +1,22 @@
 import {
     Text,
+    Box
   } from '@chakra-ui/react';
   import React from 'react';
   import { useUserContext } from "../UserContext";
   import { isPermitted } from 'shared/Role';
   import { fullPage } from 'AppPage';
-  
+  import { topbarHeight } from "../components/Navbars";
+
   export default fullPage(() => {
     const [user] = useUserContext();
     if (!isPermitted(user.roles, ["Mentee", "Mentor", "MentorCoach"])) {
       return <Text>Don't have permission to view this page.</Text>;
     }
+
+    return (
+      <Box h={`calc(100vh - ${topbarHeight})`}>
+        <iframe src="" width="100%" height="100%"/>
+      </Box>
+    )
   })
